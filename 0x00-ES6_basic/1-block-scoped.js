ab002a40807b9b460e703ff4d9c8a57de6a8c99f
Victor Abrokwah <kingportipher@outlook.com>
2024-08-06 23:38:28 +0000

    Task 1 comment

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; //Variable is local to the if block
+    const task2 = false; //Variable is local to the if block
   }
 
   return [task, task2];
