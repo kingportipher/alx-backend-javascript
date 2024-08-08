bea16351e67d9cfcce0c0f16d07aa7b2bf6d165e
Victor Abrokwah <kingportipher@outlook.com>
2024-08-08 22:51:37 +0000

    Task 2 fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,14 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then(response => {
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success'   
- };
+      return {
+        status: 200,
+        body: 'success',
+      };
     })
     .catch(() => {
+      console.log('Got a response from the API');
       return new Error();
     });
 }
