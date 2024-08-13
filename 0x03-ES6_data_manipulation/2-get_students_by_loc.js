178c20dd3d7bbe75979c672063301efacd665a1b
Victor Abrokwah <kingportipher@outlook.com>
2024-08-13 22:54:04 +0000

    Task 2 done

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,4 @@
 // 2-get_students_by_loc.js
-function getStudentsByLocation(students, city) {
+export default function getStudentsByLocation(students, city) {
   return students.filter(student => student.location === city);
 }
-
-export default getStudentsByLocation;
