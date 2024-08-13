5844a8126a0dc849eb99bc9c1bf81318162e9985
Victor Abrokwah <kingportipher@outlook.com>
2024-08-13 23:15:23 +0000

    Task 10 done

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+// 10-update_uniq_items.js
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+
+  return map;
+}
+
+export default updateUniqueItems;
