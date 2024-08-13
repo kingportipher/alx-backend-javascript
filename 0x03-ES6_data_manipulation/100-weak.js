b8849684873e4a85ea369cc4372e9d785fb1752d
Victor Abrokwah <kingportipher@outlook.com>
2024-08-13 23:16:38 +0000

    Task 100 done

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+// 100-weak.js
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const queryCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, queryCount);
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
