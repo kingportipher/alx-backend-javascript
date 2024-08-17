08d30abe86ab8738d7f4aec94139b74cb762d5bb
Victor Abrokwah <kingportipher@outlook.com>
2024-08-17 03:17:10 +0000

    Task 3 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,22 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+}
+
 interface Directors extends Teacher {
   numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
