3c47a6b607569c7238c78d02d25ada2a97b1b567
Victor Abrokwah <kingportipher@outlook.com>
2024-10-01 22:06:41 +0000

    Task 1 done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,17 @@
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
+// 1-stdin.js
 
-// Handle user input from stdin
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim(); // Get the user's input and trim whitespace
-    console.log(`Your name is: ${name}`);
-    process.exit(); // Exit the program
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-// Handle program exit event
-process.on('exit', () => {
-    console.log("This important software is now closing");
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
