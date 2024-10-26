211a12b5487367e16f0400b71b14872c19ea5408
Victor Abrokwah <kingportipher@outlook.com>
2024-10-26 10:08:41 +0000

    Task 0 fix

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,5 @@
 function calculateNumber(a, b) {
-  // Round both a and b
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-
-  // Return the sum of the rounded numbers
-  return roundedA + roundedB;
+    return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,21 @@
 const assert = require('assert');
+const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return 4 when a = 1 and b = 3', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when a = 1 and b = 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when a = 1.2 and b = 3.7', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when a = 1.5 and b = 3.7', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should return 0 when a = 0.1 and b = -0.1', () => {
-    assert.strictEqual(calculateNumber(0.1, -0.1), 0);
-  });
-
-  it('should return -4 when a = -1.4 and b = -2.6', () => {
-    assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
-  });
+    it('checking if numbers round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('checking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('checking if numbers round 3nd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('checking if numbers round 4nd round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('checking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+      });
 });
