39dd362f60b384f4ec06f3bad4bbed3e8fa1d372
Victor Abrokwah <kingportipher@outlook.com>
2024-12-02 22:05:51 +0000

    Task 0 done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,33 @@
+// 0-calcul.test.js
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum   
- of rounded numbers', () => {
+  it('should return 4 for (1, 3)', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 for (1, 3.7)', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2,   
- 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5,   
- 3.7), 6);
+  });
+
+  it('should return 5 for (1.2, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 for (1.5, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should handle negative numbers correctly', () => {
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+  });
+
+  it('should handle one positive and one negative number', () => {
+    assert.strictEqual(calculateNumber(1.7, -3.7), -2);
+  });
+
+  it('should handle zeros correctly', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
   });
 });
